f22656cd6c2ee5b15e85dc689cc91a8176982709
DeeMAZtbii <150884203+DeeMAZtbii@users.noreply.github.com>
2023-11-28 08:52:37 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -8,6 +8,17 @@ const products = [
 ];
 const gallery = document.querySelector('.gallery'); 
 
+products.forEach(product => {
+  const item = document.createElement('div');
+  item.classList.add('item');
+  item.innerHTML = `
+    <img src="${product.image}" alt="${product.name}">
+    <h3>${product.name}</h3>
+    <p>$${product.price}</p>
+  `;
+  gallery.appendChild(item);
+});
+
 const App = () => {
   const [cart, setCart] = useState([]);
 
