1080aa1a51109e96a4dc2d5bdff7baef051378e9
DeeMAZtbii <150884203+DeeMAZtbii@users.noreply.github.com>
2023-11-28 10:02:03 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
-import React, { useState } from 'react';
-
 const products = [
-  { id: 1, name: 'Футболка', price: 500 },
-  { id: 2, name: 'Джинсы', price: 1500 },
-  { id: 3, name: 'Кроссовки', price: 2000 },
-  // ...другие товары
+  { name: 'Курица с терияки', price: 20, image: 'kurter.jpg' },
+  // Другие товары
 ];
 
-const gallery = document.querySelector('.gallery'); 
+const gallery = document.querySelector('.gallery');
 
 products.forEach(product => {
   const item = document.createElement('div');
@@ -15,40 +11,7 @@ products.forEach(product => {
   item.innerHTML = `
     <img src="${product.image}" alt="${product.name}">
     <h3>${product.name}</h3>
-    <p>$${product.price}</p>
+    <p>${product.price} руб</p>
   `;
   gallery.appendChild(item);
 });
-
-const App = () => {
-  const [cart, setCart] = useState([]);
-
-  const addToCart = (product) => {
-    setCart([...cart, product]);
-  };
-
-  return (
-    <div>
-      <h1>Интернет-магазин</h1>
-      <div>
-        {products.map((product) => (
-          <div key={product.id}>
-            <h3>{product.name}</h3>
-            <p>Цена: {product.price} ₽</p>
-            <button onClick={() => addToCart(product)}>Добавить в корзину</button>
-          </div>
-        ))}
-      </div>
-      <h2>Корзина</h2>
-      {cart.length === 0 && <p>Корзина пуста</p>}
-      {cart.map((product, index) => (
-        <div key={index}>
-          <h4>{product.name}</h4>
-          <p>Цена: {product.price} ₽</p>
-        </div>
-      ))}
-    </div>
-  );
-};
-
-export default App;
